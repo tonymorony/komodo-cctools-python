703f7f35810acde990ad2d22d93669982657b083
Anton Lysakov <tlysakov@gmail.com>
2019-03-03 07:21:54 +0700

    test windows parser

--- lib/tuilib.py
+++ lib/tuilib.py
@@ -1785,8 +1785,14 @@ def check_if_config_is_here(rpc_connection):
 
 
 def find_game_keystrokes_in_log(gametxid):
-    p1 = subprocess.Popen(["cat", "keystrokes.log"], stdout=subprocess.PIPE)
-    p2 = subprocess.Popen(["grep", gametxid], stdin=p1.stdout, stdout=subprocess.PIPE)
+
+    operating_system = platform.system()
+    if operating_system == 'Win64' or operating_system == 'Windows':
+        p1 = subprocess.Popen(["type", "keystrokes.log"], stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(["findstr", gametxid], stdin=p1.stdout, stdout=subprocess.PIPE)
+    else:
+        p1 = subprocess.Popen(["cat", "keystrokes.log"], stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(["grep", gametxid], stdin=p1.stdout, stdout=subprocess.PIPE)
     p1.stdout.close()
     output = p2.communicate()[0]
     keystrokes_log_for_game = bytes.decode(output).split("\n")
