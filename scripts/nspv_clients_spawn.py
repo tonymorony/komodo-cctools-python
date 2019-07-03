9dbcb3cb66821b42e8574068e12880d80167cd5e
TonyL <tlysakov@gmail.com>
2019-07-03 15:40:31 +0300

    tweak ac params

--- scripts/nspv_clients_spawn.py
+++ scripts/nspv_clients_spawn.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 
 # init params
-nspv_clients_to_start = 10
+nspv_clients_to_start = 2000
 ac_name = 'ZEXO'
 
 # pre-creating separate folders
@@ -12,12 +12,10 @@ for i in range(nspv_clients_to_start):
     open("node_" + str(i) + "/" + ac_name + ".conf", 'a').close()
 
 #start numnodes daemons, changing folder name and port
-j = i + nspv_clients_to_start
 for i in range(nspv_clients_to_start):
-    j = j + 1
     subprocess.call(['./komodod', '-ac_name=ZEXO', '-conf=' + sys.path[0] + '/node_' + str(i) + "/" + ac_name + ".conf",
-                     '-port=' + str(7000 + i), '-rpcport=' + str(7000 + j), '-datadir=' + sys.path[0] + '/node_' + str(i),
+                     '-rpcport=' + str(7000 + i), '-datadir=' + sys.path[0] + '/node_' + str(i),
                      '-ac_supply=100000000', '-ac_reward=1478310502','-ac_halving=525600','-ac_cc=42',
                      '-ac_ccenable=236', '-ac_perc=77700', '-ac_staked=93',
                      '-ac_pubkey=02713bd85e054db923694b6b7a85306264edf4d6bd6d331814f2b40af444b3ebbc',
-                     '-ac_public=1', '-nSPV=1', '-addnode=95.217.44.58', '-daemon'])
+                     '-ac_public=1', '-nSPV=1', '-addnode=95.217.44.58', '-listen=0', '-daemon'])
