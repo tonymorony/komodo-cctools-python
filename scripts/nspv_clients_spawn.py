18b9aef73f514621ced1e899578e6a03b0931f79
TonyL <tlysakov@gmail.com>
2019-07-07 12:12:57 +0300

    added RPC proxies for nspv clients attack

--- scripts/nspv_clients_spawn.py
+++ scripts/nspv_clients_spawn.py
@@ -1,21 +1,36 @@
 import os
+import time
 import sys
 import subprocess
+from slickrpc import Proxy
 
 # init params
-nspv_clients_to_start = 2000
-ac_name = 'ZEXO'
+nspv_clients_to_start = 500
+ac_name = 'ILN'
 
 # pre-creating separate folders
 for i in range(nspv_clients_to_start):
     os.mkdir("node_" + str(i))
     open("node_" + str(i) + "/" + ac_name + ".conf", 'a').close()
+    with open("node_" + str(i) + "/" + ac_name + ".conf", 'a') as conf:
+        conf.write("rpcuser=test" + '\n')
+        conf.write("rpcpassword=test" + '\n')
+        conf.write("rpcport=" + str(7000 + i) + '\n')
 
 #start numnodes daemons, changing folder name and port
 for i in range(nspv_clients_to_start):
-    subprocess.call(['./komodod', '-ac_name=ZEXO', '-conf=' + sys.path[0] + '/node_' + str(i) + "/" + ac_name + ".conf",
+    subprocess.call(['./komodod', '-ac_name=ILN', '-conf=' + sys.path[0] + '/node_' + str(i) + "/" + ac_name + ".conf",
                      '-rpcport=' + str(7000 + i), '-datadir=' + sys.path[0] + '/node_' + str(i),
-                     '-ac_supply=100000000', '-ac_reward=1478310502','-ac_halving=525600','-ac_cc=42',
-                     '-ac_ccenable=236', '-ac_perc=77700', '-ac_staked=93',
-                     '-ac_pubkey=02713bd85e054db923694b6b7a85306264edf4d6bd6d331814f2b40af444b3ebbc',
-                     '-ac_public=1', '-nSPV=1', '-addnode=95.217.44.58', '-listen=0', '-daemon'])
+                     '-ac_supply=10000000000', '-ac_cc=2', '-nSPV=1', '-addnode=5.9.102.210', '-listen=0', '-daemon'])
+
+time.sleep(2)
+
+#creating rpc proxies for all nodes
+for i in range(nspv_clients_to_start):
+    rpcport = 7000 + i
+    globals()['proxy_%s' % i] = Proxy("http://%s:%s@127.0.0.1:%d"%("test", "test", int(rpcport)))
+
+while True:
+    for i in range(nspv_clients_to_start):
+        nspv_getinfo_output = globals()['proxy_%s' % i].nspv_getinfo()
+        print(nspv_getinfo_output)
