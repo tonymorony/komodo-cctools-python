79ac149e2db17d8d27b97859b44e49d31a26b191
SirSevenG <ottonseven@gmail.com>
2019-08-29 02:44:44 +0700

    methods fix

--- scripts/libnspv_calls.sh
+++ scripts/libnspv_calls.sh
@@ -2,9 +2,9 @@
 
 while :
 do
-  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "nspv_getinfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ &> curl-output.txt
-  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "nspv_listunspent", "params": ["RQ1mvCUcziWzRwE8Ugtex29VjoFjRzxQJT"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
-  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "nspv_notarizations", "params": ["2000"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
-  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "nspv_spentinfo", "params": ["67ffe0eaecd6081de04675c492a59090b573ee78955c4e8a85b8ac0be0e8e418", "1"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
-  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "nspv_txproof", "params": ["67ffe0eaecd6081de04675c492a59090b573ee78955c4e8a85b8ac0be0e8e418", "2673"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
+  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "getinfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ &> curl-output.txt
+  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "listunspent", "params": ["RQ1mvCUcziWzRwE8Ugtex29VjoFjRzxQJT"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
+  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "notarizations", "params": ["2000"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
+  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "spentinfo", "params": ["67ffe0eaecd6081de04675c492a59090b573ee78955c4e8a85b8ac0be0e8e418", "1"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
+  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "txproof", "params": ["67ffe0eaecd6081de04675c492a59090b573ee78955c4e8a85b8ac0be0e8e418", "2673"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
 done
