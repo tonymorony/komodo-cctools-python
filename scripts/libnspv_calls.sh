c389989c72f1d83d706ca9a9b43c824f98bb96b1
SirSevenG <ottonseven@gmail.com>
2019-08-29 02:40:43 +0700

    libnspv spam scritps

---
+++ scripts/libnspv_calls.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while :
+do
+  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "nspv_getinfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ &> curl-output.txt
+  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "nspv_listunspent", "params": ["RQ1mvCUcziWzRwE8Ugtex29VjoFjRzxQJT"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
+  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "nspv_notarizations", "params": ["2000"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
+  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "nspv_spentinfo", "params": ["67ffe0eaecd6081de04675c492a59090b573ee78955c4e8a85b8ac0be0e8e418", "1"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
+  curl -s --user test:test --data-binary '{"jsonrpc": "2.0", "id":"curltest", "method": "nspv_txproof", "params": ["67ffe0eaecd6081de04675c492a59090b573ee78955c4e8a85b8ac0be0e8e418", "2673"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1 &> curl-output.txt
+done
