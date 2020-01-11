1fed64be2a12c45c0b7e974431905cd7a2a9e723
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-01-12 00:55:43 +0700

    limit spam time

--- scripts/dexp2p/dexp2p_spam.sh
+++ scripts/dexp2p/dexp2p_spam.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-while :
-do
+end=$((SECONDS+180))
+
+while [ $SECONDS -lt $end ]; do
   TEST=$(( ( RANDOM % 1000000 )  + 1 ))
   curl -s --user test:test --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "DEX_broadcast", "params": ["'"$TEST"'", "0", "'"$1"'", "", "", "0.1", "100"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ > curl-output.txt
 done
