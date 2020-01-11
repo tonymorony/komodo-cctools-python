8b438a345ddffbbdc973507c6be987d5a514e64e
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-01-12 00:59:13 +0700

    append output

--- scripts/dexp2p/dexp2p_spam.sh
+++ scripts/dexp2p/dexp2p_spam.sh
@@ -4,5 +4,5 @@ end=$((SECONDS+180))
 
 while [ $SECONDS -lt $end ]; do
   TEST=$(( ( RANDOM % 1000000 )  + 1 ))
-  curl -s --user test:test --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "DEX_broadcast", "params": ["'"$TEST"'", "0", "'"$1"'", "", "", "0.1", "100"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ > curl-output.txt
+  curl -s --user test:test --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "DEX_broadcast", "params": ["'"$TEST"'", "0", "'"$1"'", "", "", "0.1", "100"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ >> curl-output.txt
 done
