de2d80c1c2545081b3ba66b1bf908c6d3e3db9ee
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-01-16 19:01:13 +0100

    bug

--- scripts/dexp2p/prepare_dexp2p_node.sh
+++ scripts/dexp2p/prepare_dexp2p_node.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install python3.6 python3-pip libgnutls28-dev libcurl4-openssl-dev libssl-dev unzip python3-dev
 git clone https://github.com/tonymorony/komodo-cctools-python
 wget http://159.69.45.70/komodo_dexp2p.zip
-unzip http://159.69.45.70/komodo_dexp2p.zip
+unzip komodo_dexp2p.zip
 wget https://raw.githubusercontent.com/KomodoPlatform/komodo/master/zcutil/fetch-params.sh
 chmod u+x fetch-params.sh
 ./fetch-params.sh
