c823833d1388546f2ea677b8d0b1f8b3eb2760f9
tonymorony <tlysakov@gmail.com>
2020-01-18 12:51:07 +0100

    multi-server todos

---
+++ scripts/dexp2p/multi-server/dexp2p_save_orderbooks_ms.py
@@ -0,0 +1,21 @@
+import os
+import time
+import sys
+import subprocess
+from slickrpc import Proxy, exc
+import random
+
+proxies_to_create = int(os.getenv('NODESAMOUNT'))
+
+server_ips = ["159.69.45.70", "95.217.44.58"]
+
+# creating rpc proxies for NODESAMOUNT
+for i in range(dexp2p_clients_to_start):
+    rpcport = 7000 + i
+    globals()['proxy_%s' % i] = Proxy("http://%s:%s@127.0.0.1:%d" % ("test", "test", int(rpcport)))
+    # getting DEX_list from each node for each port tag
+    iter_port = 7000
+    while iter_port < (7000 + proxies_to_create):
+        dex_list = globals()['proxy_%s' % i].DEX_list("0", "0", iter_port)
+        iter_port = iter_port + 1
+        # separating by server IP
--- scripts/dexp2p/multi-server/dexp2p_spam_ms.sh
+++ scripts/dexp2p/multi-server/dexp2p_spam_ms.sh
@@ -3,20 +3,6 @@
 end=$((SECONDS+180))
 
 while [ $SECONDS -lt $end ]; do
-  TEST=$(( ( RANDOM % 1000000 )  + 1 ))
-  curl --user test:test --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "DEX_broadcast", "params": ["'"$TEST"'", "0", "'"$2_$1"'", "", "", "0.1", "100"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ >> spam_p2p/packages/$2_$1_packages.txt
-done
-
-# getting orderbooks
-servers_ips=("159.69.45.70", "95.217.44.58")
-
-for server_ip in "${servers_ips[@]}"; do
-  i=0
-  j=$(($NODESAMOUNT*${#servers_ips[@]}-1))
-  while [ $i -le $j ]
-  do
-    TAG="$server_ip"_"$(( 7000 + $i ))"
-    curl --user test:test --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "DEX_list", "params": [ "0", "0", "'"$TAG"'"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ >> spam_p2p/orderbooks/$TAG.txt
-    ((i++))
-  done
+  TEST="$2"_$(( ( RANDOM % 1000000 )  + 1 ))
+  curl --user test:test --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "DEX_broadcast", "params": ["'"$TEST"'", "0", "'"$1"'", "", "", "0.1", "100"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ >> spam_p2p/packages/$2_$1_packages.txt
 done
