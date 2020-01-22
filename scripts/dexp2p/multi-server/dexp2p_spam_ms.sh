97a8143188c991b910a6fee0e778264a8b772130
tonymorony <tlysakov@gmail.com>
2020-01-22 17:07:02 +0100

    fix

--- scripts/dexp2p/multi-server/dexp2p_spam_ms.sh
+++ scripts/dexp2p/multi-server/dexp2p_spam_ms.sh
@@ -7,6 +7,7 @@ while [ $SECONDS -lt $end ]; do
   let "c = $i % 10"
   if (( $i == 0 ))
   then
+  TEST="$2"_$(( ( RANDOM % 1000000 )  + 1 ))
   curl --user test:test --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "DEX_broadcast", "params": ["'"$TEST"'", "0", "'"$1"'", "", "", "0.1", "100"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ >> spam_p2p/packages/$2_$1_packages.txt
   sleep 1
   i=$((i+1))
