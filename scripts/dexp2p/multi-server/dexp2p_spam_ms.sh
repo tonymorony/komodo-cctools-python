e401c9fc24ad8f0afd645ccd6ac976d4074caa0b
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-01-22 22:17:53 +0100

    test

--- scripts/dexp2p/multi-server/dexp2p_spam_ms.sh
+++ scripts/dexp2p/multi-server/dexp2p_spam_ms.sh
@@ -15,5 +15,5 @@ while [ $SECONDS -lt $end ]; do
   TEST="$2"_$(( ( RANDOM % 1000000 )  + 1 ))
   curl --user test:test --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "DEX_broadcast", "params": ["'"$TEST"'", "0", "'"$1"'", "", "", "0.1", "100"] }' -H 'content-type: text/plain;' http://127.0.0.1:$1/ >> spam_p2p/packages/$2_$1_packages.txt
   i=$((i+1))
-#  fi
+  fi
 done
