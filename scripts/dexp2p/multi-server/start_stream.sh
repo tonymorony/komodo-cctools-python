9e826352e09fe8bc2596b0ee8da8c3b44435df0c
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-02-05 02:41:49 +0700

    bug

--- scripts/dexp2p/multi-server/start_stream.sh
+++ scripts/dexp2p/multi-server/start_stream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while true; do
-  ./komodo-cli -ac_name=DEXTEST DEX_stream -conf=/root/node_0/DEXTEST.conf -datadir=/root/node_0 pony_8x01.mkv 0
+  ./komodo-cli -ac_name=DEXTEST -conf=/root/node_0/DEXTEST.conf -datadir=/root/node_ DEX_stream 0 pony_8x01.mkv 0
 done
