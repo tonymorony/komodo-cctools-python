161fd7674709b6aa671fc5c920b338a286983c1d
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2020-02-06 00:12:11 +0700

    Update watch_stream.sh

--- scripts/dexp2p/multi-server/watch_stream.sh
+++ scripts/dexp2p/multi-server/watch_stream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while true; do
-  ./komodo-cli -ac_name=DEXSTATS -conf=/root/node_0/DEXTEST.conf -rpcport=7000 -datadir=/root/node_0 DEX_streamsub pony_8x01.mkv 0 01d6a288b65dd144f054f1aaf65e4850d56eaefb559c4f2e9317e85fdabc39ac46
+  ./komodo-cli -ac_name=DEXSTATS -conf=/root/node_0/DEXTEST.conf -rpcport=7000 -datadir=/root/node_0 DEX_streamsub pony_8x01.mkv 0 $1
 done
